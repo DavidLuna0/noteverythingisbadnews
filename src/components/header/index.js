4a29f3f7893dce86800e5f41cf80770217175617
David <ddavidr569@gmail.com>
2020-04-22 16:50:23 -0300

    Changing header style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,11 +6,9 @@ export default function Header() {
   return (
     <header>
       <div className="overlay">
+        <div className="page-title">
         <h1>Nem Tudo São Más Noticías</h1>
-        <p>
-          Este site foi criado para diminuir a tensão causado pelo coronavirus
-          através da divulgação de boas noticias relacionadas ao covid-19
-        </p>
+        </div>
       </div>
     </header>
   );
