4fa74585996d38e71f49d6ac96345d74c65587cd
DavidLuna0 <ddavidr569@gmail.com>
2020-04-22 17:56:58 -0300

    Updates

---
+++ precache-manifest.deec94bd2bda4d2bf509faa2ace4c51b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4607ae9d4e9e1eb3621c4fdd9638bb87",
+    "url": "/NotEverythingIsBadNews/index.html"
+  },
+  {
+    "revision": "7a9a60152371840b8da8",
+    "url": "/NotEverythingIsBadNews/static/css/main.073d49c5.chunk.css"
+  },
+  {
+    "revision": "1bbd78ab2c85c5cac670",
+    "url": "/NotEverythingIsBadNews/static/js/2.f4573ebf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NotEverythingIsBadNews/static/js/2.f4573ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9a60152371840b8da8",
+    "url": "/NotEverythingIsBadNews/static/js/main.6d7ce29f.chunk.js"
+  },
+  {
+    "revision": "2dbbd6eecba16ff40f66",
+    "url": "/NotEverythingIsBadNews/static/js/runtime-main.ba0fd93d.js"
+  },
+  {
+    "revision": "31645ca5283fa8442bcd97816a38d5da",
+    "url": "/NotEverythingIsBadNews/static/media/BostonBlack.31645ca5.woff"
+  },
+  {
+    "revision": "00766eb7632ccc8642aec6b0a40b62b7",
+    "url": "/NotEverythingIsBadNews/static/media/BostonBlackIt.00766eb7.woff"
+  },
+  {
+    "revision": "d284681e22ab37537e5af407630b0444",
+    "url": "/NotEverythingIsBadNews/static/media/BostonBold.d284681e.woff"
+  },
+  {
+    "revision": "5ca8afe69b2c23da4602a2c4e4e6d2f2",
+    "url": "/NotEverythingIsBadNews/static/media/BostonBoldIt.5ca8afe6.woff"
+  },
+  {
+    "revision": "8990662e4be64f561f70af8ded7680c1",
+    "url": "/NotEverythingIsBadNews/static/media/BostonExtraLight.8990662e.woff"
+  },
+  {
+    "revision": "b00f4573656b9855bfebc614d160ab44",
+    "url": "/NotEverythingIsBadNews/static/media/BostonExtraLightIt.b00f4573.woff"
+  },
+  {
+    "revision": "e2e8efda807b1be6fa2b243ec030c752",
+    "url": "/NotEverythingIsBadNews/static/media/BostonHeavy.e2e8efda.woff"
+  },
+  {
+    "revision": "4d81880723b632ff4b854cfe112e9c23",
+    "url": "/NotEverythingIsBadNews/static/media/BostonHeavyIt.4d818807.woff"
+  },
+  {
+    "revision": "976418f8970fb1af428fef2ba1f99632",
+    "url": "/NotEverythingIsBadNews/static/media/BostonLight.976418f8.woff"
+  },
+  {
+    "revision": "3a1a6ff78ce94f9a1fdc8dc080f005b3",
+    "url": "/NotEverythingIsBadNews/static/media/BostonLightIt.3a1a6ff7.woff"
+  },
+  {
+    "revision": "3838b00eae5c8c7419a99c1cdb8a8b3d",
+    "url": "/NotEverythingIsBadNews/static/media/BostonRegular.3838b00e.woff"
+  },
+  {
+    "revision": "37f36c8a2a790a8f36d72783bb55f5c6",
+    "url": "/NotEverythingIsBadNews/static/media/BostonRegularIt.37f36c8a.woff"
+  },
+  {
+    "revision": "51c3750a17e2bebe1795ced9cb2ddd30",
+    "url": "/NotEverythingIsBadNews/static/media/BostonSemiBold.51c3750a.woff"
+  },
+  {
+    "revision": "ba28bc6abe0ff66c909354be5eafe393",
+    "url": "/NotEverythingIsBadNews/static/media/BostonSemiBoldIt.ba28bc6a.woff"
+  },
+  {
+    "revision": "e74bab9d209b44ebbf0d932717d8550f",
+    "url": "/NotEverythingIsBadNews/static/media/BostonThin.e74bab9d.woff"
+  },
+  {
+    "revision": "ac4fd485621d565e63fb6668b9d05331",
+    "url": "/NotEverythingIsBadNews/static/media/BostonThinIt.ac4fd485.woff"
+  },
+  {
+    "revision": "f21b49384fad5c08785474074e8c90a2",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_bold-webfont.f21b4938.woff"
+  },
+  {
+    "revision": "78e49b684e175d18db491d5cab47f838",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_bold_italic-webfont.78e49b68.woff"
+  },
+  {
+    "revision": "552cd7403ae263e0627678f336361f68",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_light-webfont.552cd740.woff"
+  },
+  {
+    "revision": "5580a3836f9a709102741d66848c8a49",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_light_italic-webfont.5580a383.woff"
+  },
+  {
+    "revision": "ef78052be53722e5c998afff6a34895f",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_regular-webfont.ef78052b.woff"
+  },
+  {
+    "revision": "b9acc6b2331599b360726e775a416eed",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_regular_italic-webfont.b9acc6b2.woff"
+  },
+  {
+    "revision": "30cbc4c6bd5a8cde3b28b1717477aea2",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_thin-webfont.30cbc4c6.woff"
+  },
+  {
+    "revision": "2b6a6b5260bb627dd45939ddbd9a8370",
+    "url": "/NotEverythingIsBadNews/static/media/bariol_thin_italic-webfont.2b6a6b52.woff"
+  }
+]);
