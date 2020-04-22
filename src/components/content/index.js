0ed72a0bd115dadcd23c22002382bab584c85e87
David <ddavidr569@gmail.com>
2020-04-22 17:04:05 -0300

    Creating Content component

---
+++ src/components/content/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Content(props) {
+  return (
+    <div className="container">
+        <h1>Main content</h1>
+    </div>
+  );
+}
