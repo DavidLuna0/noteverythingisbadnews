ddbf20fb30b9c0068943bcff69c96768e3a9405e
DavidLuna0 <ddavidr569@gmail.com>
2020-04-28 17:10:10 -0300

    Creating about component

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Grid, Card, CardContent, Typography } from "@material-ui/core";
 
-// import { Container } from './styles';
+import "./index.css";
 
-const About = () => (
-  <div>
-    <h1>Test</h1>
-  </div>
-);
-
-export default About;
+export default function About() {
+  return (
+    <Grid spacing={3} className="container">
+      <Grid item container justify="center">
+        <Card className="root-about">
+          <CardContent>
+            <Typography
+              className="about-title"
+              variant="h5"
+              color="textSecondary"
+              gutterBottom
+            >
+              O Site
+            </Typography>
+            <Typography className="about-text" variant="body1">
+              Este site foi criado com o intuito de informar a todos sobre a
+              atual situação do Covid-19 no Brasil e espalhar boas notícias a
+              fim de reduzir o estresse e a ansiedade causados por todo este
+              problema que o mundo está passando. 
+              <br/>
+              Nossa intenção é trazer um
+              sentimento de otimismo em meio a tantas noticias ruins que
+              recebemos no dia a dia
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-import {Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import MediaCard from "../card";
 
 import "./index.css";
 
-import ProgressBar from "../progress"
+import ProgressBar from "../progress";
 
 import sick from "../../assets/images/medical-mask.png";
 import recovered from "../../assets/images/protected.png";
 import brazil from "../../assets/images/brazil.png";
 
 export default function Content(props) {
-
   const activeText = "Pessoas estão atualmente infectados com o covid-19";
   const recoveredText = "Se recuperaram do covid-19 desde o inicio do surto";
   const totalText = "Foram infectados com o covid-19 desde o inicio do surto";
@@ -26,26 +25,22 @@ export default function Content(props) {
   }
 
   return (
-    <Grid
-      container
-      spacing={3}
-      className="container"
-    >
-      <Grid item container justify="center" xs>
+    <Grid container spacing={3} className="container">
+      <Grid className="content" item justify="center" xs>
         <MediaCard
           image={sick}
           value={props.numbers.active}
           text={activeText}
         ></MediaCard>
       </Grid>
-      <Grid item container justify="center" xs>
+      <Grid className="content" item justify="center" xs>
         <MediaCard
           image={recovered}
           value={props.numbers.recovered}
           text={recoveredText}
         ></MediaCard>
       </Grid>
-      <Grid item container justify="center" xs>
+      <Grid className="content" item justify="center" xs>
         <MediaCard
           image={brazil}
           value={props.numbers.total}
