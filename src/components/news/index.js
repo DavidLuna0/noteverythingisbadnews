6381bd551d45373908391334347319f2ab35138f
David <ddavidr569@gmail.com>
2020-04-28 22:21:33 -0300

    Creating news components

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import { Grid } from "@material-ui/core";
 
-// import { Container } from './styles';
+import "./index.css";
 
-const News = () => (
-  <div>
-    <h1>News</h1>
-  </div>
-);
+import NewsItem from "../newsItem";
 
-export default News;
+export default function News() {
+  
+  const [newsTitle, setNewsTitle] = useState("");
+  const [newsText, setNewsText] = useState("");
+
+  const title1 = "Titulo da Noticia 1"
+  const title2 = "Titulo da Noticia 2"
+  const text1 = "Esta é a noticia asgsagasgdagasgsgagdgfshfshdfhfhsjdasjsjsjsjsjs js jsdfh sfjsjs ";
+  const text2 = "Esta é a noticia asgsagasgdagasgsgagdgfshfshdfhfhsjdasjsjsjsjsjs js jsdfh sfjsjs 2"
+
+  return (
+    <Grid className="news-container" justify="center" container spacing={3}>
+      <NewsItem title={title1} text={text1} />
+      <NewsItem title={title2} text={text2} />
+      <NewsItem title={title1} text={text1} />
+      <NewsItem title={title2} text={text2} />
+    </Grid>
+  );
+}
