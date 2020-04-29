271a82e8803343a18a3f8a4ac8796fdb6b1f68e9
DavidLuna0 <ddavidr569@gmail.com>
2020-04-29 09:41:58 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noteverythingisbadnews/precache-manifest.573c53fd04b63df16a1b40969c152555.js"
+  "/NotEverythingIsBadNews/precache-manifest.7def5d257392b2c940ae6a21ce9dcdf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noteverythingisbadnews/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NotEverythingIsBadNews/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
